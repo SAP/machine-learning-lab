d8a7a9a9bb42e77c5a6b08ef6cf54ab0204b7e0a
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-11-14 12:01:17 +0100

    Execute tests in Kube mode when kind is installed

--- backend/lab-service/build.py
+++ backend/lab-service/build.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from universal_build import build_utils
 
 COMPONENT_NAME = "lab-service"
@@ -18,11 +21,47 @@ if args[build_utils.FLAG_MAKE]:
         build_utils.exit_process(completed_process.returncode)
 
 if args[build_utils.FLAG_TEST]:
+    build_utils.log("Run docker tests")
     completed_process = build_utils.run("mvn verify")
     if completed_process.returncode > 0:
-        build_utils.log(f"Tests failed for component {COMPONENT_NAME}")
+        build_utils.log(f"Tests failed in local mode for component {COMPONENT_NAME}")
         build_utils.exit_process(1)
+
+    if shutil.which("kind") is not None and build_utils.run("kind get clusters").stderr == '':
+        kind_cluster_name = os.getenv("kind_cluster_name", "kind")
+        build_utils.log("Run Kubernetes tests")
+        build_utils.run(
+            f"kind --name {kind_cluster_name} load docker-image lab-service:{args[build_utils.FLAG_VERSION]}",
+            exit_on_error=True,
+        )
+        build_utils.run(
+            f"kind --name {kind_cluster_name} load docker-image simple-demo-service:{args[build_utils.FLAG_VERSION]}",
+            exit_on_error=True,
+        )
+        build_utils.run(
+            f"kind --name {kind_cluster_name} load docker-image simple-demo-job:{args[build_utils.FLAG_VERSION]}",
+            exit_on_error=True,
+        )
+        completed_process = build_utils.run(
+            f"SERVICES_RUNTIME=k8s \
+            KUBE_CONFIG_PATH=kube-config \
+            LAB_DATA_ROOT=/workspace/data \
+            SERVICE_VERSION={args[build_utils.FLAG_VERSION]} \
+            IS_KIND_CLUSTER=True \
+            mvn verify"
+        )
+        if completed_process.returncode > 0:
+            build_utils.log(
+                f"Tests failed in Kubernetes mode for component {COMPONENT_NAME}"
+            )
+            build_utils.exit_process(1)
+    else:
+        build_utils.log("Skipping Kubernetes tests because kind is not installed.")
 
 # Only allow releasing from sub componenents when force flag is set as an extra precaution step
 if args[build_utils.FLAG_RELEASE] and args[build_utils.FLAG_FORCE]:
-    build_utils.release_docker_image(COMPONENT_NAME, args[build_utils.FLAG_VERSION], args[build_utils.FLAG_DOCKER_IMAGE_PREFIX])
+    build_utils.release_docker_image(
+        COMPONENT_NAME,
+        args[build_utils.FLAG_VERSION],
+        args[build_utils.FLAG_DOCKER_IMAGE_PREFIX],
+    )
