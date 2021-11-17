5dad10d3c89e0974368409d3deccf8473d1efc22
Jan Kielmann <jan.kielmann@sap.com>
2021-11-17 14:41:44 +0100

    [lab-file-browser] Delete lab-file-browser component

--- components/lab-file-browser/webapp/src/utils/config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const CONTAXY_ENDPOINT =
-  process.env.REACT_APP_CONTAXY_ENDPOINT === undefined
-    ? '/api'
-    : process.env.REACT_APP_CONTAXY_ENDPOINT;
