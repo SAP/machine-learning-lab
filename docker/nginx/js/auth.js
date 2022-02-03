a59308070d02485447673f938ed0d34ca599253e
Jan Kielmann <jan.kielmann@sap.com>
2022-02-03 15:40:56 +0100

    [ml-lab] Use contaxy endpoint to retrieve session token in nginx

--- docker/nginx/js/auth.js
+++ docker/nginx/js/auth.js
@@ -1,105 +1,123 @@
 import jwt from "js/jwt.js";
 
-function modifyToken(token) {
-  if (!token) return "";
-  return token.replace("Bearer", "").trim();
-}
-
-function verifyAccess(r, apiToken, permission) {
-  var validInSeconds = 900; // valid for 15 minutes
-  return new Promise((resolve, reject) => {
-    ngx
-      .fetch(
-        `http://127.0.0.1:8090/auth/tokens/verify?permission=${encodeURIComponent(
-          permission
-        )}`,
-        {
-          method: "POST",
-          headers: { Authorization: `Bearer ${apiToken}` },
-        }
-      )
-      .then((res) => {
-        if (res.status !== 204) {
-          reject(false);
-          return;
-        }
-        var servicePath = permission.substr(0, permission.lastIndexOf('#'));
-        var generated_session_token = jwt.generate(
-          process.env.JWT_TOKEN_SECRET,
-          undefined,
-          { perms: permission },
-          validInSeconds
-        );
-        r.headersOut[
-          "Set-Cookie"
-        ] = [
-          `ct_session_token=${generated_session_token}; HttpOnly; Path=/${servicePath}; Max-Age=${validInSeconds}`,
-          `ct_session_token=${generated_session_token}; HttpOnly; Path=/${servicePath}b; Max-Age=${validInSeconds}`
-        ]
-        resolve(true);
-      })
-      .catch((e) => {
-        r.error("NGINX: Error in verifyAccess. Details: " + e.message)
-        reject(false);
-      });
-  });
-}
-
-function isServiceIsInProject(r) {
-  var project_id = r.variables.project_id
-  var service_id = r.variables.service_id
-  var extract_project_id_regex = /-p-(?<project_id>[a-zA-Z0-9\-]+)-s-/g
-  var result = extract_project_id_regex.exec(service_id)
-  return result && "project_id" in result.groups
-         && result.groups["project_id"] == project_id
-}
-
 function guardServiceAccess(r) {
-  if(!isServiceIsInProject(r)){
+  if (!isServiceIsInProject(r)) {
     r.return(401, "Requested service is not in specified project!");
   }
-  if (r.headersIn['Authorization']) {
-    var apiToken = modifyToken(r.headersIn['Authorization'])
+
+  let apiToken;
+  if (r.headersIn["Authorization"]) {
+    apiToken = modifyToken(r.headersIn["Authorization"]);
   } else {
-    var apiToken = modifyToken(r.variables.cookie_ct_token);
+    apiToken = modifyToken(r.variables.cookie_ct_token);
   }
-  var sessionToken = modifyToken(r.variables.cookie_ct_session_token);
-  var permission = r.variables.permission;
+  const sessionToken = modifyToken(r.variables.cookie_ct_session_token);
   if (!apiToken && !sessionToken) r.return(403, "No auth cookie set");
-  var message = "";
+
+  const permission = r.variables.permission;
+  let message = "";
   if (sessionToken) {
     try {
-      var jwtPayload = jwt.verify(sessionToken, process.env.JWT_TOKEN_SECRET);
-
-      var containsPermission =
-        jwtPayload["perms"].indexOf(permission) > -1 ||
-        jwtPayload["perms"].indexOf("admin") > -1;
-      if (containsPermission) {
-        r.internalRedirect("@service");
-        return;
-      }
-
-      message = "JWT token does not contain correct permissions.";
+      verifySessionToken(r, sessionToken, permission);
+      r.internalRedirect("@service");
+      return;
     } catch (e) {
-      message = "JWT token not valid.";
+      r.error(`ERROR2: ${e}`)
+      message = e.message;
     }
   }
 
   if (apiToken) {
-    verifyAccess(r, apiToken, permission)
-      .then(
-        (res) => {
-          r.internalRedirect("@service");
-        },
-        () => r.return(403, "API Token not valid")
-      )
-      .catch((e) => {
-        r.return(403, "API Token not valid");
-      });
+    setNewSessionTokenCookie(r, apiToken, permission);
   } else {
     // Return unauthorized as no token could redirect to the service
     r.return(403, message);
   }
 }
+
+function isServiceIsInProject(r) {
+  const project_id = r.variables.project_id;
+  const service_id = r.variables.service_id;
+  const extract_project_id_regex = /-p-(?<project_id>[a-zA-Z0-9\-]+)-s-/g;
+  const result = extract_project_id_regex.exec(service_id);
+  return (
+    result &&
+    "project_id" in result.groups &&
+    result.groups["project_id"] == project_id
+  );
+}
+
+function modifyToken(token) {
+  if (!token) return "";
+  return token.replace("Bearer", "").trim();
+}
+
+
+function verifySessionToken(r, sessionToken, permission) {
+    let jwtPayload;
+    try {
+      jwtPayload = jwt.verify(sessionToken, process.env.JWT_TOKEN_SECRET);
+    } catch (e) {
+      r.error(`ERROR1: ${e}`)
+      throw new Error("JWT token not valid.");
+    }
+    const containsPermission =
+      jwtPayload["scope"].indexOf(permission) > -1 ||
+      jwtPayload["scope"].indexOf("admin") > -1;
+    if (!containsPermission) {
+      throw new Error("JWT token does not contain correct permissions.");
+    }
+}
+
+async function setNewSessionTokenCookie(r, apiToken, permission) {
+  try {
+    const token = await requestSessionToken(apiToken, permission);
+    // Set token as cookie in response:
+    const servicePath = permission.substr(0, permission.lastIndexOf("#"));
+    const validInSeconds =
+      parseInt(process.env.JWT_TOKEN_EXPIRY_MINUTES, 10) * 60 || 900; // Default is 15 minutes
+    r.headersOut["Set-Cookie"] = [
+      `ct_session_token=${token}; HttpOnly; Path=/${servicePath}; Max-Age=${
+        validInSeconds - 5 // Let cookie expire a bit earlier than token
+      }`,
+      `ct_session_token=${token}; HttpOnly; Path=/${servicePath}b; Max-Age=${
+        validInSeconds - 5
+      }`,
+    ];
+    r.internalRedirect("@service");
+  } catch (e) {
+    if (e.message) {
+      r.return(403, message);
+    } else {
+      r.return(403, "Unkonwn error while requesting session token!");
+    }
+  }
+}
+
+async function requestSessionToken(apiToken, permission) {
+  const response = await ngx.fetch(
+    `http://127.0.0.1:8090/auth/tokens?token_type=session-token&scope=${encodeURIComponent(
+      permission
+    )}`,
+    {
+      method: "POST",
+      headers: { Authorization: `Bearer ${apiToken}` },
+    }
+  );
+  if (response.status == 200) {
+    // The response body contains the JWT session token
+    return await response.json();
+  } else if (response.status >= 400 && response.status < 500) {
+    const error = await response.json();
+    throw new Error(
+      `Could not create session token! ${response.status} ${error.message}`
+    );
+  } else {
+    throw new Error(
+      `Could not create session token! ${response.status} Internal error!`
+    );
+  }
+}
+
 
 export default { guardServiceAccess };
