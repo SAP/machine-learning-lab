64e15a0e5774ea8de83802d065a2109f02b89f9d
Naveen Vishnu Kinnal <naveen.vishnu.kinnal@sap.com>
2022-04-01 13:58:40 +0200

    Changes related to AICOEPDC-87

--- client/tests/test_environment.py
+++ client/tests/test_environment.py
@@ -1,7 +1,6 @@
+from importlib.metadata import metadata
 import os
 import tempfile
-
-import contaxy.schema.exceptions
 
 from lab_client import Environment
 from .conftest import test_settings
@@ -28,7 +27,7 @@ class TestEnvironment:
                         lab_api_token='not-valid-token',
                         project=test_settings.LAB_PROJECT)
 
-    def test_upload_download(self) -> None:
+    def test_upload_download_file(self) -> None:
         env = Environment(lab_endpoint=test_settings.LAB_BACKEND,
                           lab_api_token=test_settings.LAB_TOKEN,
                           project=test_settings.LAB_PROJECT)
@@ -45,3 +44,49 @@ class TestEnvironment:
         with open(local_path, 'r') as f:
             received_file = f.read()
         assert received_file == sample_text
+
+    def test_upload_download_folder(self) -> None:
+        env = Environment(lab_endpoint=test_settings.LAB_BACKEND,
+                          lab_api_token=test_settings.LAB_TOKEN,
+                          project=test_settings.LAB_PROJECT)
+        tf_dir = tempfile.TemporaryDirectory()
+
+        tf_file = 'test.txt'
+        sample_text = "This is a sample text\nWith two lines"
+        with open(os.path.join(tf_dir.name, tf_file), 'w') as f:
+            f.write(sample_text)
+            f.seek(0)
+        
+        file_key = env.upload_folder(tf_dir.name, "dataset")
+        assert file_key == f"datasets/{tf_dir.name.split(os.sep)[-1]}.zip"
+
+        #Test to download zip file as a folder and return the folder path with unpack=True
+        local_path = env.get_file(file_key, unpack=True)
+        assert os.path.exists(local_path)
+        
+        for _, _, files in os.walk(local_path):
+            for filename in files:
+                assert filename == tf_file
+
+        #Test to upload folder with custom name
+        file_key = env.upload_folder(tf_dir.name, "dataset", None, "abc")
+        assert file_key == f"datasets/abc.zip"
+    
+    def test_upload_download_file_metadata(self) -> None:
+        env = Environment(lab_endpoint=test_settings.LAB_BACKEND,
+                          lab_api_token=test_settings.LAB_TOKEN,
+                          project=test_settings.LAB_PROJECT)
+        tf = tempfile.NamedTemporaryFile()
+        sample_text = "This is a sample text\nWith two lines"
+        metadata_input = dict()
+        metadata_input['description'] = 'This is test file'
+        with open(tf.name, 'w') as f:
+            f.write(sample_text)
+            f.seek(0)
+            file_key = env.upload_file(tf.name, "dataset", metadata=metadata_input)
+        assert file_key == f"datasets/{tf.name.split(os.sep)[-1]}"
+
+        file_metadata = env.get_file_metadata(env.project, file_key)
+        for key, value in file_metadata.items():
+            if 'description' in key:
+                assert value == metadata_input['description']
