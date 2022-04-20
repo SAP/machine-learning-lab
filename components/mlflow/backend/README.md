f9b66308c68f4105db6b827c3fa91d7329a86b7f
zohair <Zohair.ul.hasan@gmail.com>
2022-04-20 10:43:39 -0400

    first attempt at adding new component to ML LAB

---
+++ components/mlflow/backend/README.md
@@ -0,0 +1,2 @@
+# ML Lab Template Component
+The backend is build as its own python package and uses fastapi + uvicorn for the REST API.
---
+++ components/mlflow/backend/src/mlflow/utils.py
@@ -0,0 +1,39 @@
+import os
+from typing import Generator
+
+from contaxy.utils.auth_utils import get_api_token
+from contaxy.clients import (
+    AuthClient,
+    DeploymentClient,
+    ExtensionClient,
+    FileClient,
+    JsonDocumentClient,
+    ProjectClient,
+    SystemClient,
+)
+from contaxy.clients.shared import BaseUrlSession
+from contaxy.managers.components import ComponentManager
+from fastapi import Depends, Request
+
+CONTAXY_API_ENDPOINT = os.getenv("CONTAXY_API_ENDPOINT", None)
+
+
+def get_component_manager(
+    request: Request,
+    token: str = Depends(get_api_token),
+) -> Generator[ComponentManager, None, None]:
+    """Returns the initialized component manager.
+
+    This is used as FastAPI dependency and called for every request.
+    """
+    session = BaseUrlSession(base_url=CONTAXY_API_ENDPOINT)
+    session.headers = {"Authorization": f"Bearer {token}"}
+    with ComponentManager(request) as component_manager:
+        component_manager._auth_manager = AuthClient(session)
+        component_manager._extension_manager = ExtensionClient(session)
+        component_manager._project_manager = ProjectClient(session)
+        component_manager._system_manager = SystemClient(session)
+        component_manager._json_db_manager = JsonDocumentClient(session)
+        component_manager._deployment_manager = DeploymentClient(session)
+        component_manager._file_manager = FileClient(session)
+        yield component_manager
