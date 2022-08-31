68f18bbd094012d968e6fbdee5e0b325f9282637
zohair <Zohair.ul.hasan@gmail.com>
2022-08-31 12:27:04 -0400

    added option to refresh logs every five seconds automatically

--- webapp/src/components/Dialogs/LogsDialog.jsx
+++ webapp/src/components/Dialogs/LogsDialog.jsx
@@ -3,11 +3,14 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
+import Checkbox from '@mui/material/Checkbox';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
 
 import { servicesApi } from '../../services/contaxy-api';
 
@@ -15,6 +18,7 @@ function LogsDialog(props) {
   const { onClose, title, projectId, serviceId } = props;
 
   const [logs, setLogs] = useState(null);
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
     servicesApi.getServiceLogs(projectId, serviceId).then((l) => {
@@ -32,11 +36,36 @@ function LogsDialog(props) {
     setLogs(await servicesApi.getServiceLogs(projectId, serviceId));
   };
 
+  const handleCheck = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  useEffect(() => {
+    let intervalId = null;
+    if (checked) {
+      intervalId = setInterval(() => {
+        servicesApi.getServiceLogs(projectId, serviceId).then((l) => {
+          setLogs(l);
+        });
+      }, 5000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [checked, projectId, serviceId]);
+
   return (
     <Dialog open>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>{contentElement}</DialogContent>
       <DialogActions>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={checked} onChange={handleCheck} />}
+            label="Refresh automatically every 5 seconds"
+          />
+        </FormGroup>
         <Button onClick={onRefresh} color="primary">
           REFRESH
         </Button>
