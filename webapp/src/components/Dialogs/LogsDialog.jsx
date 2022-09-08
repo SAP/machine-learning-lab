f2a9e77126c17a1f12d2df17e06249c63f42cccf
zohair <Zohair.ul.hasan@gmail.com>
2022-09-08 09:48:12 -0400

    removed mui and used material-ui instead

--- webapp/src/components/Dialogs/LogsDialog.jsx
+++ webapp/src/components/Dialogs/LogsDialog.jsx
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
-import Checkbox from '@mui/material/Checkbox';
+import Checkbox from '@material-ui/core/Checkbox';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
 
 import { servicesApi } from '../../services/contaxy-api';
 
