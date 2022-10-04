cc501c2a3fdd83aa8ecc31d54dd2143b3278595b
zohair <Zohair.ul.hasan@gmail.com>
2022-10-04 12:17:02 -0400

    refactored tests to make them more easier to read

--- components/lab-job-scheduler/backend/tests/test_job_deployer.py
+++ components/lab-job-scheduler/backend/tests/test_job_deployer.py
@@ -1,3 +1,4 @@
+from turtle import pu
 import pytest
 
 import lab_job_scheduler.job_deployer as jd
@@ -7,47 +8,74 @@ import uuid
 import datetime
 
 
+@pytest.fixture
+def job_input():
+    return JobInput(container_image="hello-world", display_name="HelloWorld")
+
+
+@pytest.fixture
+def job_without_last_run(job_input: JobInput):
+    return ScheduledJob(
+        job_id=str(uuid.uuid4()),
+        created=datetime.datetime(2021, 1, 1, 0, 0, 0).isoformat(),
+        last_run=None,
+        cron_string="0 1 * * *",
+        job_input=job_input
+    )
+
+
+@pytest.fixture
+def job_with_last_run(job_input: JobInput):
+    return ScheduledJob(
+        job_id=str(uuid.uuid4()),
+        created=datetime.datetime(2020, 12, 31, 0, 0, 0).isoformat(),
+        last_run=datetime.datetime(2021, 1, 1, 1, 0, 0).isoformat(),
+        cron_string="0 1 * * *",
+        job_input=job_input
+    )
+
+
 @pytest.mark.unit
-class TestJobDeployer:
-    def test_run_scheduled_job(self):
-        date = datetime.datetime(2021, 1, 1, hour=0, minute=0, second=0, microsecond=0)
-        job_input = JobInput(container_image="hello-world", display_name="HelloWorld")
-        job = ScheduledJob(
-            job_id=str(uuid.uuid4()),
-            created=date.isoformat(),
-            last_run=None,
-            cron_string="0 1 * * *",
-            job_input=job_input
-        )
-        assert not jd.is_due(job, date)
-        assert not jd.is_due(job, datetime.datetime(
+class TestJobDeployerWithoutLastRun:
+    def test_is_job_due_before_creation_of_job(self, job_without_last_run: ScheduledJob):
+        assert not jd.is_due(job_without_last_run, datetime.datetime(
             2020, 1, 1, hour=0, minute=0, second=0, microsecond=0))
-        assert not jd.is_due(job, datetime.datetime(
+
+    def test_is_job_due_on_creation_of_job(self, job_without_last_run: ScheduledJob):
+        assert not jd.is_due(job_without_last_run, datetime.datetime(
+            2021, 1, 1, hour=0, minute=0, microsecond=0))
+
+    def test_is_job_due_before_next_scheduled_run_time(self, job_without_last_run: ScheduledJob):
+        assert not jd.is_due(job_without_last_run, datetime.datetime(
             2021, 1, 1, hour=0, minute=59, second=59, microsecond=0))
-        assert jd.is_due(job, datetime.datetime(
+
+    def test_is_job_due_at_next_scheduled_run_time(self, job_without_last_run: ScheduledJob):
+        assert jd.is_due(job_without_last_run, datetime.datetime(
             2021, 1, 1, hour=1, minute=0, second=0, microsecond=0))
-        assert jd.is_due(job, datetime.datetime(
+
+    def test_is_job_due_after_next_scheduled_run_time(self, job_without_last_run: ScheduledJob):
+        assert jd.is_due(job_without_last_run, datetime.datetime(
             2021, 1, 1, hour=1, minute=0, second=1, microsecond=0))
 
-    def test_run_job_that_has_run_before(self):
-        created_date = datetime.datetime(
-            2020, 12, 31, hour=0, minute=0, second=0, microsecond=0)
-        last_run_date = datetime.datetime(
-            2021, 1, 1, hour=1, minute=0, second=0, microsecond=0)
-        job_input = JobInput(container_image="hello-world", display_name="HelloWorld")
-        job = ScheduledJob(
-            job_id=str(uuid.uuid4()),
-            created=created_date.isoformat(),
-            last_run=last_run_date.isoformat(),
-            cron_string="0 1 * * *",
-            job_input=job_input
-        )
-        assert not jd.is_due(job, last_run_date)
-        assert not jd.is_due(job, datetime.datetime(
+
+@pytest.mark.unit
+class TestJobDeployerWithLastRun:
+    def test_is_job_due_at_last_run_time(self, job_with_last_run: ScheduledJob):
+        assert not jd.is_due(job_with_last_run, datetime.datetime(
+            2021, 1, 1, hour=1, minute=0, second=0, microsecond=0))
+
+    def test_is_job_due_before_creation_of_job(self, job_with_last_run: ScheduledJob):
+        assert not jd.is_due(job_with_last_run, datetime.datetime(
             2020, 1, 1, hour=0, minute=0, second=0, microsecond=0))
-        assert not jd.is_due(job, datetime.datetime(
+
+    def test_is_job_due_before_next_scheduled_run_time(self, job_with_last_run: ScheduledJob):
+        assert not jd.is_due(job_with_last_run, datetime.datetime(
             2021, 1, 2, hour=0, minute=59, second=59, microsecond=0))
-        assert jd.is_due(job, datetime.datetime(
+
+    def test_is_job_due_at_next_scheduled_run_time(self, job_with_last_run: ScheduledJob):
+        assert jd.is_due(job_with_last_run, datetime.datetime(
             2021, 1, 2, hour=1, minute=0, second=0, microsecond=0))
-        assert jd.is_due(job, datetime.datetime(
+
+    def test_is_job_due_after_next_scheduled_run_time(self, job_with_last_run: ScheduledJob):
+        assert jd.is_due(job_with_last_run, datetime.datetime(
             2021, 1, 2, hour=1, minute=0, second=1, microsecond=0))
